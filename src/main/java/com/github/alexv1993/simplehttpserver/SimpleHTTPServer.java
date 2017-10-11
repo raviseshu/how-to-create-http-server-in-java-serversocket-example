edc951e4b5f6e1fe0457e1ce9d77a2a359302343
Vendin Alex <alexvendin@gmail.com>
2017-10-11 17:27:55 +0300

    adding accept connection from client

--- src/main/java/com/github/alexv1993/simplehttpserver/SimpleHTTPServer.java
+++ src/main/java/com/github/alexv1993/simplehttpserver/SimpleHTTPServer.java
@@ -4,17 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by vendin on 11.10.2017.
  */
 public class SimpleHTTPServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
         final ServerSocket server = (ServerSocket) applicationContext.getBean("server");
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-
+            final Socket client = server.accept();
         }
 
     }
