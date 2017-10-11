1531ba79b6ffcc3f4f474f3815c3bd81a95ad713
Vendin Alex <alexvendin@gmail.com>
2017-10-11 17:40:38 +0300

    Realisation of request and response

--- src/main/java/com/github/alexv1993/simplehttpserver/SimpleHTTPServer.java
+++ src/main/java/com/github/alexv1993/simplehttpserver/SimpleHTTPServer.java
@@ -3,8 +3,11 @@ package com.github.alexv1993.simplehttpserver;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * Created by vendin on 11.10.2017.
@@ -15,7 +18,22 @@ public class SimpleHTTPServer {
         final ServerSocket server = (ServerSocket) applicationContext.getBean("server");
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            final Socket client = server.accept();
+
+
+            try (Socket clientSocket = server.accept()) {
+                InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                String line = reader.readLine();
+                while (!line.isEmpty()) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+
+            }
         }
 
     }
